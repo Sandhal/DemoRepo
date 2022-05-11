90b231751c10140eaca2b2589c9fa4feaf3ec1d8
Sandhal <61999346+Sandhal@users.noreply.github.com>
2022-05-11 05:24:08 -0700

    Update closeBrowser.java
    new code added to the close browser file

--- DemoGitRepoTest1/src/main/java/com/qa/pages/closeBrowser.java
+++ DemoGitRepoTest1/src/main/java/com/qa/pages/closeBrowser.java
@@ -5,5 +5,7 @@ public class closeBrowser {
 	public void closeBrowser() {
 		System.out.println("Close the browser");
 	}
-
+        public void openBrowser(){
+		System.out.println("Open brpwser");
+		
 }
